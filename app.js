13ca941c2cf9f972753b3e02b69c2bd7039372dd
VinnnyD22 <vincedipiero@yahoo.com>
2014-07-03 10:17:51 -0700

    updated app.js
    Button Is not working anymore

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-	alert('pages are linked')
+	alert('pages are linked');
 	var txtbox = $(document).getElementById('item');
-	var txtval = txbox.val();
+	var txtval = txtbox.val;
 
 	$('button').click(function () {
-		alert('click is working');
-      	//$('.list-background').append('<div class="list"><img class="x" src="X.png"></div>');
-     });
+		alert('button works');
+		$('.list-background').append('<div class="list"><img class="x" src="X.png"></div>')
+	});
 
      $('.list-background').on('click', '.list', function() {
      	$(this).append('<div class="checkmark"><img class="check" src="checkmark.png"></div>');
