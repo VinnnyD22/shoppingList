50155ea1e7f25aa81d68d6ad66dc78f067be863d
VinnnyD22 <vincedipiero@yahoo.com>
2014-07-02 18:33:04 -0700

    Update app.js
    added variable and changed both functions to one line.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
+	var txtbox = $(document).getElementBlyd('item');
+	var txtval = txbox.val();
 	$('button').click(function () {
-      	$('.list-background').append('<div class="list"></div>');
-    	$('.list').last().append('<img class="x" src="../shoppingList/X.png">');
+      	$('.list-background').append('<div class="list"><p>+ txtval +</p><img class="x" src="../shoppingList/X.png"></div>');
      });
 
      $('.list-background').on('click', '.list', function() {
-     	$(this).append('<div class="checkmark"></div>');
-      	$('.checkmark').last().append('<img class="check" src="../shoppingList/checkmark.png">');
+     	$(this).append('<div class="checkmark"><img class="check" src="../shoppingList/checkmark.png</div>');
     });
       
 });
